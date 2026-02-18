8ff54997687604e340fb0c42c1e0956f21f06792
Laury PRIN <prinlauryy@gmail.com>
2026-02-18 15:11:33 +0100

    Mise a jour du SplashScreen.kt pour qu'il s'adapte aux tablettes et aux telephones

--- app/src/main/java/com/openclassroom/joiefull/ui/screens/SplashScreen.kt
+++ app/src/main/java/com/openclassroom/joiefull/ui/screens/SplashScreen.kt
@@ -2,7 +2,7 @@ package com.openclassroom.joiefull.ui.screens
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
@@ -16,28 +16,35 @@ import com.openclassroom.joiefull.ui.theme.JoiefullTheme
 import com.openclassroom.joiefull.ui.theme.Orange
 
 
-
 @Composable
-    fun SplashScreen(modifier: Modifier = Modifier) {
-        Box(modifier = modifier
+fun SplashScreen(modifier: Modifier = Modifier) {
+
+    BoxWithConstraints(
+        modifier = modifier
             .background(Orange),
-            contentAlignment = Alignment.Center,
-        ){
-            Image(
-                painter = painterResource(id = R.drawable.ic_app_name),
-                contentDescription = null,
-                modifier = Modifier
-                    .height(50.dp)
-                    .width(199.dp)
-            )
-        }
+        contentAlignment = Alignment.Center,
+    ) {
+        // Adaptation de la taille du logo par rapport à la taille de l'écran
+        val logoFraction = if (maxWidth < 600.dp) 0.5f else 0.3f
+        val logoWidth = (maxWidth * logoFraction)
+        val logoHeight = logoWidth / 4f
+
+        Image(
+            painter = painterResource(id = R.drawable.ic_app_name),
+            contentDescription = null,
+            modifier = Modifier
+                .width(logoWidth)
+                .height(logoHeight)
+        )
+
     }
+}
 
 
-    @Preview(showBackground = true)
-    @Composable
-    fun SplashScreenPreview() {
-        JoiefullTheme {
-            SplashScreen()
-        }
+@Preview(showBackground = true)
+@Composable
+fun SplashScreenPreview() {
+    JoiefullTheme {
+        SplashScreen()
     }
+}
